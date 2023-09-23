52ceb80f7405c8e0ccab3354ddc56abd26ce46b2
pooulad <86445458+pooulad@users.noreply.github.com>
2023-09-23 14:33:04 +0330

    Update Shop.tsx

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,12 +1,15 @@
 import { Row, Col } from "react-bootstrap";
 import productItems from "../data/products.json";
+import Product from "../components/Product";
 
 function Shop() {
   return (
     <>
       <Row md={2} xs={1} lg={3} className="g-3">
         {productItems.map((item) => (
-          <Col>{}</Col>
+          <Col key={item.id}>
+            <Product {...item} />
+          </Col>
         ))}
       </Row>
     </>

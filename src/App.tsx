53702a9acf318cb290ea7d0c3b7b906a9872eee6
pooulad <86445458+pooulad@users.noreply.github.com>
2023-09-23 10:17:46 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+
 function App() {
   return (
-    <div>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
   );
 }
 

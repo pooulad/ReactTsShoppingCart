06fcc8eeceb0aa0fcc0711ba8d8560d07513f3c0
pooulad <86445458+pooulad@users.noreply.github.com>
2023-09-23 10:53:39 +0330

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Container, Navbar as NavbarBs, Nav } from "react-bootstrap";
+import { Container, Navbar as NavbarBs, Nav, Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
@@ -6,16 +6,27 @@ function Navbar() {
     <NavbarBs className="bg-dark text-light mb-3">
       <Container>
         <Nav className="me-auto">
-          <Nav.Link to="/" as={NavLink}>
+          <Nav.Link className="text-light" to="/" as={NavLink}>
             Home
           </Nav.Link>
-          <Nav.Link to="/shop" as={NavLink}>
+          <Nav.Link className="text-light" to="/shop" as={NavLink}>
             Shop
           </Nav.Link>
-          <Nav.Link to="/about" as={NavLink}>
+          <Nav.Link className="text-light" to="/about" as={NavLink}>
             About
           </Nav.Link>
         </Nav>
+        <Button
+          variant="outline-light"
+          style={{
+            width: "3rem",
+            height: "3rem",
+            position: "relative",
+            fontSize: "1.2rem",
+          }}
+        >
+          <i className="bi bi-cart"></i>
+        </Button>
       </Container>
     </NavbarBs>
   );

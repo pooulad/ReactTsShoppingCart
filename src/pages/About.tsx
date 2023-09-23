36388e15591eb43f04de75d9d45aefe11431e401
pooulad <86445458+pooulad@users.noreply.github.com>
2023-09-23 10:17:38 +0330

    Create About.tsx

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <div>
+      <div>About</div>
+      <div>About</div>
+      <div>About</div>
+      <div>About</div>
+    </div>
+  );
+}
+
+export default About;

137d15d3fc037786d5d86f72a50003ab031cedc9
pooulad <86445458+pooulad@users.noreply.github.com>
2023-09-23 10:42:17 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 

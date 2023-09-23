9a298c07e4eeb1fb73400dc48acc9f9dbcb074d3
pooulad <86445458+pooulad@users.noreply.github.com>
2023-09-23 10:46:35 +0330

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,22 @@
-import { Container, Navbar as NavbarBs } from "react-bootstrap";
+import { Container, Navbar as NavbarBs, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <NavbarBs className="bg-dark text-light mb-3">
-      <Container>test</Container>
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/shop" as={NavLink}>
+            Shop
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+      </Container>
     </NavbarBs>
   );
 }

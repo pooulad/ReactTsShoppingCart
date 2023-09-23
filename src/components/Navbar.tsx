5e96b7e45dbf5d0acaa85f191e14fd1f14b71380
pooulad <86445458+pooulad@users.noreply.github.com>
2023-09-23 10:42:19 +0330

    Create Navbar.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Container, Navbar as NavbarBs } from "react-bootstrap";
+
+function Navbar() {
+  return (
+    <NavbarBs className="bg-dark text-light mb-3">
+      <Container>test</Container>
+    </NavbarBs>
+  );
+}
+
+export default Navbar;

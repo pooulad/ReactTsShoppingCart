5aa6ad237cb8d33ab6a552ee5d3e3188e0136341
pooulad <86445458+pooulad@users.noreply.github.com>
2023-09-23 16:44:28 +0330

    Update Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -5,3 +5,15 @@ type CartProps = {
   isOpen: boolean;
 };
 
+function Cart({ isOpen }: CartProps) {
+  const { closeCart } = useCartContext();
+  return (
+    <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+    </Offcanvas>
+  );
+}
+
+export default Cart;

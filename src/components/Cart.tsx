392441a152d1851befdbf23d6536eefec5cc1340
pooulad <86445458+pooulad@users.noreply.github.com>
2023-09-23 17:05:10 +0330

    Update Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,6 +1,7 @@
 import { useCartContext } from "../context/CartContext";
 import { Offcanvas, Stack } from "react-bootstrap";
 import CartItem from "./CartItem";
+import productItems from "../data/products.json";
 
 type CartProps = {
   isOpen: boolean;
@@ -18,6 +19,15 @@ function Cart({ isOpen }: CartProps) {
           {cartItems.map((item) => (
             <CartItem key={item.id} {...item} />
           ))}
+          <div className="fw-bold fs-5 text-dark">
+            Total :{" "}
+            {cartItems.reduce((total, currentItem) => {
+              const product = productItems.find(
+                (item) => item.id === currentItem.id
+              );
+              return total + (product?.price || 0) * currentItem.qty;
+            }, 0)}
+          </div>
         </Stack>
       </Offcanvas.Body>
     </Offcanvas>
